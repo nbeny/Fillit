a0aa310e59bb50405317374287e7b77c4b660258
Nicolas BENY <nbeny@e1r10p6.42.fr>
2016-11-24 01:28:37 +0100

    debloque pas trop Kbagot j ai sorti du lourd

--- buffer.c
+++ buffer.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,40 +7,77 @@
 /*   By: nbeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 13:04:45 by nbeny             #+#    #+#             */
-/*   Updated: 2016/11/23 19:29:35 by nbeny            ###   ########.fr       */
+/*   Updated: 2016/11/24 01:25:48 by nbeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
-static int	**ft_reader(t_list **alst)
+#include <stdio.h>
+t_var	*init_tvar(t_var *var)
 {
-	t_var a;
-	int		fd;
-	char	buffer[21];
-	t_list	lst;
+	var->a = 0;
+	var->b = 0;
+	var->i = 0;
+	var->j = 0;
+	var->k = 0;
+	var->l = 0;
+	return (var);
+}
 
-	lst = (*alst);
-	lst = ft_lstnew(**tab, size);
-	a.j = 0;
-	fd = open(argv[1], O_RDONLY);
-	while (read(fd, buffer, 21) != 0)
+t_buff	*init_tbuff(t_buff *buff, char **av)
+{
+	buff->fd = open(av[1], O_RDONLY);
+	return (buff);
+}
+
+int		**ft_reader(int ac, char **av)
+{
+	t_var	*var;
+	t_buff	*buff;
+	int		**tab;
+
+	buff = (t_buff *)malloc(sizeof(t_buff));
+	var = (t_var *)malloc(sizeof(t_var));
+	buff = init_tbuff(buff, av);
+	var = init_tvar(var);
+	tab = (int **)malloc(sizeof(int *) * 50);
+	while (var->l < 49)
 	{
-		a.k = 0;
-		while (a.k < 20)
+		tab[var->l] = (int *)malloc(sizeof(int) * 4);
+		var->l++;
+	}
+	tab[var->l] = NULL;
+	while (read(buff->fd, buff->buff, 21) != 0)
+	{
+		var->k = 0;
+		while (var->k < 21)
 		{
-			if (buffer[k] == '.')
-					lst->tab[j][i++] = 0;
-			if (buffer[k] == '#')
-					lst->tab[j][i++] = 1;
-			if (buffer[k] == '\n')
+			if (buff->buff[var->k] == '.')
 			{
-				i = 0;
-				j++;
+				tab[var->j][var->i] = 0;
+				printf("%d ", tab[var->j][var->i]);fflush(stdout);
+				var->i++;
 			}
+			if (buff->buff[var->k] == '#')
+			{
+				tab[var->j][var->i] = 1;
+				printf("%d ", tab[var->j][var->i]);
+				var->i++;
+			}
+			if (buff->buff[var->k] == '\n')
+			{
+				var->i = 0;
+				var->j++;
+				printf("\n");
+			}
+			var->k++;
 		}
-		ft_lstadd(lst->tab, lst->size);
-
 	}
 	return (tab);
+}
+
+int		main(int ac, char **av)
+{
+	ft_reader(ac, av);
+	return(0);
 }
--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: nbeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 13:08:19 by nbeny             #+#    #+#             */
-/*   Updated: 2016/11/23 19:11:00 by nbeny            ###   ########.fr       */
+/*   Updated: 2016/11/24 00:44:25 by nbeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,15 @@
 # include <stdlib.h>
 # include <fcntl.h>
 
+typedef struct	s_buff
+{
+	int		fd;
+	char	buff[21];
+}				t_buff;
 typedef struct	s_var
 {
+	int a;
+	int b;
 	int i;
 	int j;
 	int k;
@@ -30,11 +37,11 @@ typedef struct	s_list
 	size_t			size;
 	struct s_list	*next;
 }				t_list;
-t_list			*ft_lstnew(void const **tab, size_t size);
-void			ft_lstdelone(t_list **alst, void (*del)(void **, size_t));
-void			ft_lstdel(t_list **alst, void (*del)(void **, size_t));
-void			ft_lstadd(t_list **alst, t_list *new);
-void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+//t_list			*ft_lstnew(void const **tab, size_t size);
+//void			ft_lstdelone(t_list **alst, void (*del)(void **, size_t));
+//void			ft_lstdel(t_list **alst, void (*del)(void **, size_t));
+//void			ft_lstadd(t_list **alst, t_list *new);
+//void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+//t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
